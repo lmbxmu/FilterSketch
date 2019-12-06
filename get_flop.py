d44b5c6bac308bcedd0911be10535dae90a14434
shaojieli <shaojieli@stu.xmu.edu.cn>
2019-12-06 21:43:07 +0800

    add getflops method

--- get_flop.py
+++ get_flop.py
@@ -1,9 +1,8 @@
-
 import torch
 import argparse
-import get_flops
 import utils.common as utils
 from importlib import import_module
+from thop import profile
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 
@@ -39,24 +38,41 @@ device = torch.device("cpu")
 print('==> Building model..')
 sketch_rate = utils.get_sketch_rate(args.sketch_rate)
 if args.arch == 'vgg':
+    orimodel = import_module(f'model.{args.arch}').VGG().to(device)
     model = import_module(f'model.{args.arch}').SketchVGG(sketch_rate, start_conv=1).to(device)
 elif args.arch == 'resnet':
     if args.data_set == 'imagenet':
+        orimodel = import_module(f'model.{args.arch}_imagenet')\
+                    .resnet(args.cfg).to(device)
         model = import_module(f'model.{args.arch}_imagenet')\
                     .resnet(args.cfg, sketch_rate=sketch_rate, start_conv=1).to(device)
     else:
+        orimodel = import_module(f'model.{args.arch}').resnet(args.cfg).to(device)
         model = import_module(f'model.{args.arch}')\
                     .resnet(args.cfg, sketch_rate=sketch_rate, start_conv=1).to(device)
 elif args.arch == 'googlenet':
+    orimodel = import_module(f'model.{args.arch}').googlenet().to(device)
     model = import_module(f'model.{args.arch}').googlenet(sketch_rate).to(device)
 elif args.arch == 'densenet':
+    orimodel = import_module(f'model.{args.arch}').densenet_cifar().to(device)
     model = import_module(f'model.{args.arch}').densenet_cifar(sketch_rate).to(device)
-
-if args.arch=='googlenet' or args.arch=='resnet_50':
-    flops, params = get_flops.measure_model(model, device, 3, args.input_image_size, args.input_image_size, True)
 else:
-    flops, params= get_flops.measure_model(model, device, 3, args.input_image_size, args.input_image_size)
+    raise ('arch not exist!')
 
+input = torch.randn(1, 3, args.input_image_size, args.input_image_size)
+
+print('--------------UnPrune Model--------------')
+oriflops, oriparams = profile(orimodel, inputs=(input, ))
+print('Params: %.2f'%(oriparams))
+print('FLOPS: %.2f'%(oriflops))
+
+print('--------------Prune Model--------------')
+flops, params = profile(model, inputs=(input, ))
 print('Params: %.2f'%(params))
-print('Flops: %.2f'%(flops))
+print('FLOPS: %.2f'%(flops))
+
+print('--------------Compress Rate--------------')
+print('Params Compress Rate: %d/%d (%.2f%%)' % (params, oriparams, 100. * params / oriparams))
+print('FLOPS Compress Rate: %d/%d (%.2f%%)' % (flops, oriflops, 100. * flops / oriflops))
+
 
